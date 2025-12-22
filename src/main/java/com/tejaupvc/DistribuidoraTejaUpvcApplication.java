9ac4778d033af91ae2768d5f0a43766a3ce20b1f
CEAS <ceas881@gmail.com>
2025-12-21 22:25:09 -0500

    Fix datasource and JPA configuration

--- src/main/java/com/tejaupvc/DistribuidoraTejaUpvcApplication.java
+++ src/main/java/com/tejaupvc/DistribuidoraTejaUpvcApplication.java
@@ -2,9 +2,13 @@ package com.tejaupvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.tejaupvc.repository")
+@EntityScan(basePackages = "com.tejaupvc.model")
 public class DistribuidoraTejaUpvcApplication {
     public static void main(String[] args) {
         SpringApplication.run(DistribuidoraTejaUpvcApplication.class, args);
