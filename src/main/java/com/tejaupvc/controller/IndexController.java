0ddadec1440d51627126c142343043bb1c0f2e72
CEAS <ceas881@gmail.com>
2025-12-28 22:07:32 -0500

    Fix lectura de imagenes desde classpath

--- src/main/java/com/tejaupvc/controller/IndexController.java
+++ src/main/java/com/tejaupvc/controller/IndexController.java
@@ -3,13 +3,12 @@ package com.tejaupvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.io.File;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
 
 @Controller
 public class IndexController {
@@ -19,13 +18,18 @@ public class IndexController {
 
         List<String> imagenes = new ArrayList<>();
 
-        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        PathMatchingResourcePatternResolver resolver =
+                new PathMatchingResourcePatternResolver();
 
-        Resource[] resources = resolver.getResources("classpath:/static/img/productos/*");
+        Resource[] resources = resolver.getResources(
+                "classpath:static/img/productos/*"
+        );
 
         for (Resource resource : resources) {
-            String nombre = resource.getFilename();
-            imagenes.add("/img/productos/" + nombre);
+            String filename = resource.getFilename();
+            if (filename != null) {
+                imagenes.add("/img/productos/" + filename);
+            }
         }
 
         model.addAttribute("imagenes", imagenes);
